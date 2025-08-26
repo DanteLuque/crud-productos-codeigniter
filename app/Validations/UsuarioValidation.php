9a61110f0fdcc400b034ab2a72773be421a3a936
DanDev1004 <dandeveloper1004@gmail.com>
2025-08-26 17:19:21 -0500

    refactor: 💡 si usa mis custom msg, nomas tenia q especificar xd

--- app/Validations/UsuarioValidation.php
+++ app/Validations/UsuarioValidation.php
@@ -5,10 +5,10 @@ namespace App\Validations;
 class UsuarioValidation
 {
     public array $usuario = [
-        'nombres'    => 'required|min_length[2]|max_length[255]',
-        'apellidos'  => 'required|min_length[2]|max_length[255]',
-        'tipo_doi_id'=> 'required|is_natural_no_zero',
-        'num_doi'    => 'required|min_length[5]|max_length[45]',
+        'nombres'    => 'required|min_length[2]',
+        'apellidos'  => 'required|min_length[2]',
+        'tipo_doi_id' => 'required|is_natural_no_zero',
+        'num_doi'    => 'required',
         'username'   => 'required|min_length[4]|max_length[70]|is_unique[usuarios.username,id,{id}]',
         'userpass'   => 'required|min_length[6]',
     ];
@@ -16,9 +16,11 @@ class UsuarioValidation
     public array $usuario_errors = [
         'nombres' => [
             'required' => 'El nombre es obligatorio',
+            'min_length' => 'El nombre debe tener mínimo 2 caracteres',
         ],
         'apellidos' => [
             'required' => 'El apellido es obligatorio',
+            'min_length' => 'El apellido debe tener mínimo 2 caracteres',
         ],
         'tipo_doi_id' => [
             'required' => 'Debe seleccionar un tipo de documento',
@@ -29,6 +31,8 @@ class UsuarioValidation
         'username' => [
             'required' => 'El usuario es obligatorio',
             'is_unique' => 'Este nombre de usuario ya está registrado',
+            'min_length' => 'El usuario debe tener mínimo 4 caracteres',
+            'max_length' => 'El usuario no puede superar 70 caracteres',
         ],
         'userpass' => [
             'required' => 'Debe ingresar una contraseña',
